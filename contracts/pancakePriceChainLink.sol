ee7efb420b453a16035d493cc1b6d26bb759d411
curtdk <942233@qq.com>
2023-04-08 11:12:05 +0800

    222

--- contracts/pancakePriceChainLink.sol
+++ contracts/pancakePriceChainLink.sol
@@ -1,73 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
+
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@OpenZeppelin/contracts-v0.7/math/SafeMath.sol";
 
-
-
-
-
-interface IPancakePair {
-    event Approval(address indexed owner, address indexed spender, uint value);
-    event Transfer(address indexed from, address indexed to, uint value);
-
-    function name() external pure returns (string memory);
-    function symbol() external pure returns (string memory);
-    function decimals() external pure returns (uint8);
-    function totalSupply() external view returns (uint);
-    function balanceOf(address owner) external view returns (uint);
-    function allowance(address owner, address spender) external view returns (uint);
-
-    function approve(address spender, uint value) external returns (bool);
-    function transfer(address to, uint value) external returns (bool);
-    function transferFrom(address from, address to, uint value) external returns (bool);
-
-    function DOMAIN_SEPARATOR() external view returns (bytes32);
-    function PERMIT_TYPEHASH() external pure returns (bytes32);
-    function nonces(address owner) external view returns (uint);
-
-    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
-
-    event Mint(address indexed sender, uint amount0, uint amount1);
-    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
-    event Swap(
-        address indexed sender,
-        uint amount0In,
-        uint amount1In,
-        uint amount0Out,
-        uint amount1Out,
-        address indexed to
-    );
-    event Sync(uint112 reserve0, uint112 reserve1);
-
-    function MINIMUM_LIQUIDITY() external pure returns (uint);
-    function factory() external view returns (address);
-    function token0() external view returns (address);
-    function token1() external view returns (address);
-    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
-    function price0CumulativeLast() external view returns (uint);
-    function price1CumulativeLast() external view returns (uint);
-    function kLast() external view returns (uint);
-
-    function mint(address to) external returns (uint liquidity);
-    function burn(address to) external returns (uint amount0, uint amount1);
-    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
-    function skim(address to) external;
-    function sync() external;
-
-    function initialize(address, address) external;
-}
-
-
-//接口
-interface IPancakeRouter {
-    function quote(uint amountA, uint reserveA, uint reserveB) external pure returns (uint amountB);
-    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) external pure returns (uint amountOut);
-    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) external pure returns (uint amountIn);
-    function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
-    function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts);
-}
+import '@uniswap/v2-core/contracts/interfaces/IPancakeRouter.sol';
 
 contract PriceConsumerV3 is Ownable {
     mapping(address => AggregatorV3Interface) internal priceFeedMap;
@@ -90,30 +27,24 @@ contract PriceConsumerV3 is Ownable {
         return price;
     }
 
-    function getETHPx(address pair) external view override returns (uint) {
-
-        uint256 price=IPancakeRouter("0x10ed43c718714eb63d5aa57b78b54704e256024e").lpPrice();
-
-        address token0 = IPancakePair(pair).token0();
-        address token1 = IPancakePair(pair).token1();
-        uint totalSupply = IPancakePair(pair).totalSupply();
-        (uint r0, uint r1, ) = IPancakePair(pair).getReserves();
-        uint sqrtK = HomoraMath.sqrt(r0.mul(r1)).fdiv(totalSupply); // in 2**112
-
-        uint px0 = base.getETHPx(token0); // in 2**112
-        uint px1 = base.getETHPx(token1); // in 2**112
-
-        // fair token0 amt: sqrtK * sqrt(px1/px0)
-        // fair token1 amt: sqrtK * sqrt(px0/px1)
-        // fair lp price = 2 * sqrt(px0 * px1)
-        // split into 2 sqrts multiplication to prevent uint overflow (note the 2**112)
-        return sqrtK.mul(2).mul(HomoraMath.sqrt(px0)).div(2**56).mul(HomoraMath.sqrt(px1)).div(2**56);
-
-  }
 
 
-
-
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
+    require(tokenA != tokenB, 'UniswapV2: IDENTICAL_ADDRESSES');
+    (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+    require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');
+    require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
+    bytes memory bytecode = type(UniswapV2Pair).creationCode;
+    bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+    assembly {
+        pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+    }
+    IUniswapV2Pair(pair).initialize(token0, token1);
+    getPair[token0][token1] = pair;
+    getPair[token1][token0] = pair; // populate mapping in the reverse direction
+    allPairs.push(pair);
+    emit PairCreated(token0, token1, pair, allPairs.length);
+}
 
 
 
